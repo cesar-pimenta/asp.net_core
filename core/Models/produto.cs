bde3cace8b41d99ba69e37c51f8c9d1f715c8613
cesar-pimenta <cesar-pimenta@tuta.io>
2019-09-06 19:05:25 -0300

    pass

--- core/Models/produto.cs
+++ core/Models/produto.cs
@@ -6,12 +6,7 @@ namespace core.Models
     {
         public int Id { get; set; }
         public string Descricao { get; set; }
-
-<<<<<<< HEAD
-        
-=======
         public List<Atributo> Atributos { get; set;}
 
->>>>>>> 4f7f8f6501799ba3001eb1272fb4d0db8f81c736
     }
 }
